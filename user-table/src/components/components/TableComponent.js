28333ae0ed1cb549ae5bf9ed75eff3a963e63343
Lewis Barron <lewis.barron@fairfaxmedia.co.nz>
2018-06-15 10:54:10 +1200

    Pulling json feed data into table rows.

--- user-table/src/components/components/TableComponent.js
+++ user-table/src/components/components/TableComponent.js
@@ -8,38 +8,41 @@ require('styles/components/Table.scss');
 const users = require('json-loader!../../data/users.json');
 
 const userTotal = Object.keys(users).length;
+const userData = [];
 
-for (var count = 0; count <= userTotal; count++) {
-  console.log('User is: ', users[count]);
+for (var count = 1; count <= userTotal; count++) {
+  userData.push(
+    <tr>
+      <th scope="row">{count}</th>
+      <td>{users[count].firstName}</td>
+      <td>{users[count].surname}</td>
+      <td className={`td-sex td-sex--${users[count].sex}`}><img src={`images/icon_${users[count].sex}.svg`} alt={users[count].sex} /></td>
+      <td className={`td-tier td-tier--${users[count].tier}`}><img src={`images/tier_${users[count].tier}.svg`} alt={users[count].tier} /></td>
+      <td><a href={`mailto:{users[count].email}`}>{users[count].email}</a></td>
+    </tr>
+  );
 }
 
-class TableComponent extends React.Component {
-  render() {
-    return (
-      <table className="table" aria-label="Table of users">
-        <thead>
-          <tr>
-            <th className="th1" scope="col"></th>
-            <th scope="col">First Name</th>
-            <th scope="col">Surname</th>
-            <th scope="col">Sex</th>
-            <th scope="col">Tier</th>
-            <th scope="col">Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Fred</td>
-            <td>Redwood</td>
-            <td className="td-sex td-sex--male"><img src="images/icon_male.svg" alt="Male" /></td>
-            <td className="td-tier td-tier--gold"><img src="images/tier_gold.svg" alt="Gold" /></td>
-            <td><a href="mailto:fred@gmail.com">fred@gmail.com</a></td>
-          </tr>
-        </tbody>
-      </table>
-    );
-  }
+console.log(userData);
+
+function TableComponent() {
+  return (
+    <table className="table" aria-label="Table of users">
+      <thead>
+        <tr>
+          <th className="th1" scope="col"></th>
+          <th scope="col">First Name</th>
+          <th scope="col">Surname</th>
+          <th scope="col">Sex</th>
+          <th scope="col">Tier</th>
+          <th scope="col">Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {userData}
+      </tbody>
+    </table>
+  );
 }
 
 TableComponent.displayName = 'ComponentsTableComponent';
